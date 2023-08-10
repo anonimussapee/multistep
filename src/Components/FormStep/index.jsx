0974f8e423c397584b6ef5ebae47ebd0c86f021b
jdx <librededrogas420@gmail.com>
2023-08-09 23:05:00 -0500

    i added 2 steps with his contents

---
+++ src/Components/FormStep/index.jsx
@@ -0,0 +1,134 @@
+import { useState } from 'react'
+
+const Form1 = () => {
+  return (
+    <form onSubmit={'none'}  className='w-[90%] h-[100%] overflow-y-scroll text-gray-500  flex flex-col gap-2 py-10'>
+      <h2 className='text-[2.4rem] font-extrabold text-[--marine_blue] '>Personal info</h2>
+      <p className='line-normal'>Please provide your name, email, address, and phone number. </p>
+      <label htmlFor="customer_name">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Name</span>
+        <input type="text" name='customer_name' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g. Stephen King'/>
+      </label>
+      <label htmlFor="customer_email">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Email Address</span>
+        <input type="text" name='customer_email' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g. stephenking@lorem.com'/>
+      </label>
+      <label htmlFor="customer_phone">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Name</span>
+        <input type="text" name='customer_phone' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g.+1 234 567 890'/>
+      </label>
+    </form>
+  )
+}
+
+const Form2 = () => {
+
+  const [active, setActive] = useState({
+    op1:true,
+    op2:false,
+    op3:false
+  })
+
+  const [plans, setPlans] = useState({mm:true,yy:false})
+  return (
+    <form onSubmit={'none'}  className='w-[90%] h-[100%] overflow-y-scroll text-gray-500  flex flex-col gap-4 py-10'>
+      <h2 className='text-[2.4rem] font-extrabold text-[--marine_blue] '>Select your plan</h2>
+      <p className='line-normal'>You have the option of monthly or yearly billing </p>
+      {/* this are a options plan */}
+      <div className='flex flex-col gap-6 my-2'>
+        <label className={`flex gap-6 items-center border-[2px] ${active.op1 ? 'border-[--marine_blue]' : 'border-gray-300' }  rounded-xl p-4`} onClick={()=>setActive({
+            op1:true,
+            op2:false,
+            op3:false
+          })}>
+          <span className='arcade img_2 w-[4.5rem] h-[4.5rem]' ></span>
+          <div className='flex flex-col '>
+            <span className='text-[--marine_blue] text-[1.6rem] font-bold'>Arcade</span>
+            {
+              plans.mm ? (<span className='text-[1.6rem] '>$9/mo</span>) : (<><span className='text-[1.6rem] '>$90/yr</span><span className='text-[--marine_blue] text-[1.6rem] font-bold'>2 months free</span></>)
+            }
+          </div>      
+        </label>
+        <label className={`flex gap-6 items-center border-[2px] ${active.op2 ? 'border-[--marine_blue]' : 'border-gray-300' }  rounded-xl p-4`} onClick={()=>setActive({
+            op1:false,
+            op2:true,
+            op3:false
+          })}>
+          <span className='advanced img_2 w-[4.5rem] h-[4.5rem]'></span>
+          <div className='flex flex-col '>
+            <span className='text-[--marine_blue] text-[1.6rem] font-bold'>Advanced</span>
+            {
+              plans.mm ? (<span className='text-[1.6rem] '>$12/yr</span>) : (<><span className='text-[1.6rem] '>$120/yr</span><span className='text-[--marine_blue] text-[1.6rem] font-bold'>2 months free</span></>)
+            }
+          </div>      
+        </label>
+        <label className={`flex gap-6 items-center border-[2px] ${active.op3 ? 'border-[--marine_blue]' : 'border-gray-300' }  rounded-xl p-4`} onClick={()=>setActive({
+            op1:false,
+            op2:false,
+            op3:true
+          })}>
+          <span className='pro img_2 w-[4.5rem] h-[4.5rem]'></span>
+          <div className='flex flex-col '>
+            <span className='text-[--marine_blue] text-[1.6rem] font-bold'>Pro</span>
+            {
+              plans.mm ? (<span className='text-[1.6rem] '>$15/yr</span>) : (<><span className='text-[1.6rem] '>$150/yr</span><span className='text-[--marine_blue] text-[1.6rem] font-bold'>2 months free</span></>)
+            }
+          </div>      
+        </label>
+      </div>
+      {/* change plans toggle */}
+      <div className='w-[250px] flex justify-evenly relative font-bold'>
+       <span className={`${plans.mm ? 'text-[--marine_blue]': ''}`}>Monthly</span>
+       <div className={`w-[37px] h-[2.2rem] rounded-2xl bg-[--marine_blue] flex items-center px-2 ${plans.mm ? 'justify-start': 'justify-end'} relative `}>
+         <span className='bg-white w-[15px] h-[15px] block rounded-full' onClick={()=>{
+          setPlans({mm:!plans.mm, yy:!plans.yy })
+         }}></span> 
+       </div> 
+       <span className={`${plans.yy ? 'text-[--marine_blue]': ''}`}>Yearly</span>
+      </div>
+    </form>
+  )
+}
+
+const Form3 = () => {
+  return ( 
+    <form onSubmit={'none'}  className='w-[90%] h-[100%] overflow-y-scroll text-gray-500  flex flex-col gap-2 py-10'>
+      <h2 className='text-[2.4rem] font-extrabold text-[--marine_blue] '>Personal info</h2>
+      <p className='line-normal'>Please provide your name, email, address, and phone number. </p>
+      <label htmlFor="customer_name">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Name</span>
+        <input type="text" name='customer_name' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g. Stephen King'/>
+      </label>
+      <label htmlFor="customer_email">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Email Address</span>
+        <input type="text" name='customer_email' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g. stephenking@lorem.com'/>
+      </label>
+      <label htmlFor="customer_phone">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Name</span>
+        <input type="text" name='customer_phone' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g.+1 234 567 890'/>
+      </label>
+    </form>
+  )
+}
+
+const Form4 = () => {
+  return (
+    <form onSubmit={'none'}  className='w-[90%] h-[100%] overflow-y-scroll text-gray-500  flex flex-col gap-2 py-10'>
+      <h2 className='text-[2.4rem] font-extrabold text-[--marine_blue] '>Personal info</h2>
+      <p className='line-normal'>Please provide your name, email, address, and phone number. </p>
+      <label htmlFor="customer_name">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Name</span>
+        <input type="text" name='customer_name' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g. Stephen King'/>
+      </label>
+      <label htmlFor="customer_email">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Email Address</span>
+        <input type="text" name='customer_email' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g. stephenking@lorem.com'/>
+      </label>
+      <label htmlFor="customer_phone">
+        <span className='text-[--marine_blue] text-[1.6rem]'>Name</span>
+        <input type="text" name='customer_phone' className='border-[1px] border-gray-400 w-[100%] p-3 rounded-lg ' placeholder='e.g.+1 234 567 890'/>
+      </label>
+    </form>
+  )
+}
+export {Form1, Form2, Form3,Form4}
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,17 +1,29 @@
 import './App.css'
-import bgSlidebarMb from '../../assets/bg-sidebar-mobile.svg'
 import { Steps } from '../../Components/Steps'
 import { useSteps } from '../../Components/useSteps'
+import { Form1, Form2, Form3, Form4 } from '../../Components/FormStep'
 
 const App = () => {
 
   const {step, onStep1, onStep2, onStep3, onStep4} = useSteps()
+
+  const formSteps = {
+    step1 :<Form1/>,
+    step2 :<Form2/>,
+    step3 :<Form3/>,
+    step4 :<Form4/>,
+
+  }
+  
   return (
     <section className='w-full h-[100vh] text-[1.8rem]'>
-      <div className='main--all-container'>
+      {/* this container have all elements and have the responsability of will change of mediaqueries */}
+      <div className='main--all-container relative'>
+        {/* this container have a buttons and header image */}
         <div className='main--slidebar-container relative'>
-          <img src={bgSlidebarMb} alt="fondo white" className='min-w-[320px] w-[100vw] max-w-[700px] '/>
-          <div className='buttons-container flex justify-evenly gap-10  smMax:w-[300px] smMax:h-[65px] border-[2px] border-black'>
+          <div className=' principal_image img_2 min-w-[320px] w-[100vw] max-w-[700px] h-[200px]'></div>
+          {/* this container have a few buttons to complete a necessary form */}
+          <div className='buttons-container flex justify-center gap-10  smMax:w-[300px] smMax:h-[65px]'>
             
             <Steps step={step} btnAction={onStep1} number={1} stepNumber={'Step 1'} stepName={'YOUR INFO'}/>
             <Steps step={step} btnAction={onStep2} number={2} stepNumber={'Step 2'} stepName={'SELECT PLAN'}/>
@@ -19,6 +31,10 @@ const App = () => {
             <Steps step={step} btnAction={onStep4} number={4} stepNumber={'Step 4'} stepName={'SUMMARY'}/>
 
           </div>
+
+        </div>
+        <div className='form-Container'>
+          {formSteps[(Object.keys(step))[((Object.values(step)).findIndex(item => item===true))]]}
         </div>
       </div>
     
